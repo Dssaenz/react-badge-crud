20912d4ea8e7ae7f65e6fb38b66a4b8b69d433c2
Dssaenz <darwinsaenz105@gmail.com>
2020-05-04 01:31:01 -0500

    add new item

--- src/components/BadgesList/index.js
+++ src/components/BadgesList/index.js
@@ -53,19 +53,6 @@ function BadgesList(props) {
         <ListContainer>
         {filterResults.map(item => {
         return <BadgeCard item={item} />
-          // <li key={item.id} className='Badge__list'>
-          //   <Link to={`/badges/${item.id}`}>
-          //     <div>
-          //       <img src={item.avatarUrl} alt={item.firstName} className='Badge__image' />
-          //       <div>
-          //         <p className='Badge__list--name'>{item.firstName} {item.lastName}</p>
-          //         <i className='fab fa-github' />
-          //         <a href={`https://github.com/${item.gitHub}`} className='Badge__social'>{item.gitHub}</a>
-          //         <p className='Badge__list--git'>{item.jobTitle}</p>
-          //       </div>
-          //     </div>
-          //   </Link>
-          // </li>
       })}
       </ListContainer> 
       </PrincipalContainer>
